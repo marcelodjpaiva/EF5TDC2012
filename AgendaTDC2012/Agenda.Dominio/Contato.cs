b1640ef2dbef3bf485c55aa799b6eb9705a81e70
Marcelo Paiva <marcelo@softprime.com.br>
2012-10-19 21:02:47 -0300

    inserindo projeto

---
+++ AgendaTDC2012/Agenda.Dominio/Contato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations; 
+
+namespace Agenda.Dominio
+{
+    [Table("Pessoas")]
+    public class Contato
+    {
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        [StringLength(100)]
+        public string Endereco { get; set; }
+
+        [StringLength(20)]
+        public string Telefone { get; set; }
+    }
+}
